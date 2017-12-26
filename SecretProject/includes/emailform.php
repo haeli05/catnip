61af76cef63a79bc01dc57c18542d2ec3a462eea
haeli05 <havoc795@hotmail.com>
2017-12-27 02:47:28 +0800

    email

--- SecretProject/includes/emailform.php
+++ SecretProject/includes/emailform.php
@@ -13,7 +13,7 @@
 	$email_body = "You have received a new message. \n\n".
 				  "Email: $email \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: enquiries@catnip.fun\n";
+	$headers .= "From: catnip@catnip.fun\n";
 	$headers .= "Reply-To: $email";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
